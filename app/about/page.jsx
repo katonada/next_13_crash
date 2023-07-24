03b9c1207b8682cd0a1d0768be3746fa07033160
katonada <davor.katona.sesto@wm.rs>
2023-07-24 13:38:26 +0200

    add few more pages

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About 600code</h1>
+            <p>Whether you’re a complete newbie, or a seasoned veteran in the industry, I'm confident you'll find something useful here.
+            </p></div>
+    );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TeamPage = () => {
+    return (
+        <div>
+            <h1>Team Page</h1>
+            <p>I'm eager to share my 23 years of experience working on projects ranging from small-scale businesses to large-scale systems and web portals.</p>
+        </div>
+    );
+};
+
+export default TeamPage;
