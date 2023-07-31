6cd633cce096b1cdf2d70c37773071a04da07cb3
katonada <davor.katona.sesto@wm.rs>
2023-07-31 14:16:33 +0200

    add layout page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,7 +5,8 @@ const AboutPage = () => {
         <div>
             <h1>About 600code</h1>
             <p>Whether you’re a complete newbie, or a seasoned veteran in the industry, I'm confident you'll find something useful here.
-            </p></div>
+            </p>
+        </div>
     );
 };
 
