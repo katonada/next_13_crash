0f1edf956021edd59ca1aef284421d2bcab2653d
katonada <davor.katona.sesto@wm.rs>
2023-08-02 12:09:09 +0200

    add api filter by query

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
 import Link from 'next/link';
+import Courses from './components/Courses';
 
 const HomePage = () => {
     return (
         <div>
             <h1>Wellcome to 600Code</h1>
-            <ul>
+            <Courses></Courses>
+            {/* <ul>
                 <li><Link href='/'>Home</Link></li>
                 <li><Link href='/about'>About</Link></li>
                 <li><Link href='/about/team'>Team</Link></li>
-            </ul>
+            </ul> */}
         </div>
     );
 };
