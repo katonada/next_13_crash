ce2ca9e2f92b23c31b4640b5457fde01b46cecac
katonada <davor.katona.sesto@wm.rs>
2023-08-02 13:02:57 +0200

    add course by POST to API

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,25 @@
 import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
 import courses from './data.json';
 
 export async function GET(req) {
+    return NextResponse.json(courses);
+}
+
+
+export async function POST(req) {
+
+    const { title, description, level, link } = await req.json();
+
+    const newCourse = {
+        id: uuidv4,
+        title,
+        description,
+        level,
+        link
+    };
+
+    courses.push(newCourse);
+
     return NextResponse.json(courses);
 }
