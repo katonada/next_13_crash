cadcd3d21584dd7ac6d403ef7b1299e84aacfc26
katonada <davor.katona.sesto@wm.rs>
2023-08-02 15:31:37 +0200

    change Courses component from server to client

--- app/components/Courses.jsx
+++ app/components/Courses.jsx
@@ -1,13 +1,6 @@
 import Link from 'next/link';
 
-async function fetchCourses() {
-    const res = await fetch('http://127.0.0.1:3000/api/courses');
-    const courses = res.json();
-    return courses;
-}
-
-const Courses = async () => {
-    const courses = await fetchCourses();
+const Courses = async ({ courses }) => {
     return (
         <div className='courses'>
             {courses.map((course) => (
--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,34 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import loadingPage from './loading';
 import Courses from './components/Courses';
 
 const HomePage = () => {
+
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const res = await fetch('/api/courses');
+            const data = await res.json();
+
+            setCourses(data);
+            setLoading(false);
+        };
+
+        fetchCourses();
+    }, []);
+
+    if (loading) {
+        return <loadingPage/>;
+    }
+
     return (
         <div>
             <h1>Wellcome to 600Code</h1>
-            <Courses></Courses>
-            {/* <ul>
-                <li><Link href='/'>Home</Link></li>
-                <li><Link href='/about'>About</Link></li>
-                <li><Link href='/about/team'>Team</Link></li>
-            </ul> */}
+            <Courses courses={courses}/>
         </div>
     );
 };
